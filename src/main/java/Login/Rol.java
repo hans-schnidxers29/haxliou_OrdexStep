13051e0c1e2d9d030d5a46b4b572779e8e84a1a0
hans-schnidxers29 <hanks.patino64@gmail.com>
2025-10-24 22:50:28 -0500

    mas modificaiones en las nuevas clases

--- src/main/java/Login/Rol.java
+++ src/main/java/Login/Rol.java
@@ -1,4 +1,54 @@
 package Login;
 
-public class Rol {
+import jakarta.persistence.*;
+
+@Entity
+public class Rol{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @OneToOne(mappedBy = "rol")
+    private Usuario usuarios;
+
+
+    public Rol(){}
+
+    public Rol(Usuario usuarios, String nombre) {
+        this.usuarios = usuarios;
+        this.nombre = nombre;
+    }
+
+    public Rol(String nombre){
+        this.nombre = nombre;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Usuario getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Usuario usuarios) {
+        this.usuarios = usuarios;
+    }
 }
