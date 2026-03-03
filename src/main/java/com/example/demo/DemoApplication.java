a36d9080c9d16523281763e7ec367717fca543f3
hans-schnidxers29 <hanks.patino64@gmail.com>
2026-03-02 19:27:02 -0500

    errores de fechas

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoApplication  {
 
+	@PostConstruct
+	public void init() {
+		// Fuerza a que toda la JVM use la hora de Colombia independientemente de donde esté el servidor de AWS
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
